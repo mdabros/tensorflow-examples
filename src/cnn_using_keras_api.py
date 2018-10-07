9f07f7cee7ad0e59fce85e1610512313e2ab3024
mdabros <mdabros@users.noreply.github.com>
2018-10-07 10:50:44 +0200

    Update cnn using keras api to use cats and dogs data as example

--- src/cnn_using_keras_api.py
+++ src/cnn_using_keras_api.py
@@ -1,66 +1,77 @@
 import tensorflow as tf
-import tensorflow_hub as hub
-import numpy as np
 import os
 
 tf.logging.set_verbosity(tf.logging.INFO)
 
 def main(unused_argv):
 
-	# the data, split between train and test sets
-	mnist = tf.keras.datasets.mnist
-	(x_train, y_train), (x_test, y_test) = mnist.load_data()
+	# All images will be rescaled by 1./255
+	train_datagen = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1./255)
+	validation_datagen = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1./255)
+	test_datagen = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1./255)
 
-	# input image dimensions
-	img_rows, img_cols = 28, 28
+	train_generator = train_datagen.flow_from_directory(
+		# This is the target directory
+		r'E:\DataSets\CatsAndDogs\train\train_small',
+		# All images will be resized to 150x150
+		target_size=(150, 150),
+		batch_size=20,
+		# Since we use binary_crossentropy loss, we need binary labels
+		class_mode='binary')
 
-	x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
-	x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
-	input_shape = (img_rows, img_cols, 1)
+	validation_generator = validation_datagen.flow_from_directory(
+		r'E:\DataSets\CatsAndDogs\train\validation',
+		target_size=(150, 150),
+		batch_size=20,
+		class_mode='binary')
+
+	test_generator = validation_datagen.flow_from_directory(
+		r'E:\DataSets\CatsAndDogs\train\test',
+		target_size=(150, 150),
+		batch_size=20,
+		class_mode='binary')
+
 
 	model = tf.keras.models.Sequential()
-
-	# Conv1 section
-	model.add(tf.keras.layers.Conv2D(32, (3, 3), padding = 'same', input_shape=input_shape))
-	model.add(tf.keras.layers.ReLU())
-	model.add(tf.keras.layers.MaxPool2D((2, 2), strides = (2, 2)))
-
-	# Conv2 section
-	model.add(tf.keras.layers.Conv2D(32, (3, 3), padding = 'same'))
-	model.add(tf.keras.layers.ReLU())
-	model.add(tf.keras.layers.MaxPool2D((2, 2), strides = (2, 2)))
 	
-	# Dense section
+	model.add(tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(150, 150, 3)))
+	model.add(tf.keras.layers.MaxPooling2D((2, 2)))
+	model.add(tf.keras.layers.Conv2D(64, (3, 3), activation='relu'))
+	model.add(tf.keras.layers.MaxPooling2D((2, 2)))
+	model.add(tf.keras.layers.Conv2D(128, (3, 3), activation='relu'))
+	model.add(tf.keras.layers.MaxPooling2D((2, 2)))
+	model.add(tf.keras.layers.Conv2D(128, (3, 3), activation='relu'))
+	model.add(tf.keras.layers.MaxPooling2D((2, 2)))
 	model.add(tf.keras.layers.Flatten())
-	model.add(tf.keras.layers.Dense(256))
-	model.add(tf.keras.layers.ReLU())
-	model.add(tf.keras.layers.Dropout(0.4))
+	model.add(tf.keras.layers.Dense(512, activation='relu'))
+	model.add(tf.keras.layers.Dense(1, activation='sigmoid'))
 
-	# output layer 10 classes
-	model.add(tf.keras.layers.Dense(10))
-	model.add(tf.keras.layers.Softmax())
-
-	model.compile(optimizer = tf.keras.optimizers.Adam(0.01),
-				  loss = tf.keras.losses.sparse_categorical_crossentropy,
+	model.compile(optimizer = tf.keras.optimizers.RMSprop(lr=1e-4),
+				  loss = tf.keras.losses.binary_crossentropy,
 				  metrics=['accuracy'])
 
 	# print model architecture
 	print(model.summary())
 
-	model.fit(x_train, y_train, epochs=1, batch_size=32)
-	
-	# print test loss and metric
-	print(model.evaluate(x_test, y_test))
-	
+	#model.fit(x_train, y_train, epochs=1, batch_size=32)
+	model.fit_generator(
+		train_generator,
+		steps_per_epoch=100,
+		epochs=30,
+		validation_data=validation_generator,
+		validation_steps=50)
+
+	print(model.evaluate_generator(test_generator))
+
 	# save model
-	model_name = 'mnist_model.h5'
+	model_name = 'cats_and_dogs_small_model.h5'
 	model.save(model_name)
 
 	# load model. This fails.
 	loaded_model = tf.keras.models.load_model(model_name)
 
 	# print test loss and metric for loaded model
-	print(loaded_model.evaluate(x_test, y_test))
+	print(loaded_model.evaluate_generator(test_generator))
 
 if __name__ == "__main__":
 	tf.app.run()
