54229c1db12274b9bf6d2460f16a07c4369098e4
mdabros <mdabros@users.noreply.github.com>
2018-10-07 11:31:51 +0200

    Update transfer learning example to use cats and dogs dataset with generators

--- src/transfer_learning_using_keras_applications.py
+++ src/transfer_learning_using_keras_applications.py
@@ -4,27 +4,44 @@ import numpy as np
 
 tf.logging.set_verbosity(tf.logging.INFO)
 
-def adjust_image(data):
-    # Reshaped to [batch, height, width, channels].
-    imgs = tf.reshape(data, [-1, 28, 28, 1])
-    # Adjust image size to that of the transfer learning model.
-    imgs = tf.image.resize_images(imgs, (48, 48))
-    # Convert to RGB image.
-    imgs = tf.image.grayscale_to_rgb(imgs)
-    return imgs
-
 def main(unused_argv):
 	
-	# the data, split between train and test sets
-	mnist = tf.keras.datasets.mnist
-	(x_train, y_train), (x_test, y_test) = mnist.load_data()
+	# All images will be rescaled by 1./255, 
+	# and random augmentation are added to the training generator
+	train_datagen = tf.keras.preprocessing.image.ImageDataGenerator(
+		rescale=1./255,
+		rotation_range=40,
+		width_shift_range=0.2,
+		height_shift_range=0.2,
+		shear_range=0.2,
+		zoom_range=0.2,
+		horizontal_flip=True,
+		fill_mode='nearest')
 
-	# input image dimensions
-	img_rows, img_cols = 48, 48
+	# Note that validation and test generators must not use augmentations!
+	validation_datagen = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1./255)
+	test_datagen = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1./255)
 
-	x_train = adjust_image(x_train)#x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
-	x_test = adjust_image(x_test)#x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
-	input_shape = (img_rows, img_cols, 3)
+	train_generator = train_datagen.flow_from_directory(
+		# This is the target directory
+		r'E:\DataSets\CatsAndDogs\train\train_small',
+		# All images will be resized to 150x150
+		target_size=(150, 150),
+		batch_size=20,
+		# Since we use binary_crossentropy loss, we need binary labels
+		class_mode='binary')
+
+	validation_generator = validation_datagen.flow_from_directory(
+		r'E:\DataSets\CatsAndDogs\train\validation',
+		target_size=(150, 150),
+		batch_size=20,
+		class_mode='binary')
+
+	test_generator = test_datagen.flow_from_directory(
+		r'E:\DataSets\CatsAndDogs\train\test',
+		target_size=(150, 150),
+		batch_size=20,
+		class_mode='binary')
 
 	# Load the transfer learning model from the model hub.
 	# model hub is currently not compatible with tf.keras for fine tuning. 
@@ -34,46 +51,52 @@ def main(unused_argv):
 	# load vgg16 without final dense layers.
 	vgg16_model = applications.VGG16(weights='imagenet', 
 		include_top=False, 
-		input_shape = input_shape)
+		input_shape = (150, 150, 3))
 	
+	# Print pretrained architecture.
+	print(vgg16_model.summary())
+
 	## create new dense layers as final classifier.
 	model = tf.keras.Sequential()
 	model.add(vgg16_model)
 	model.add(tf.keras.layers.Flatten())
-	model.add(tf.keras.layers.Dense(256))
-	model.add(tf.keras.layers.ReLU())
+	model.add(tf.keras.layers.Dense(256, activation='relu'))
 	model.add(tf.keras.layers.Dropout(0.5))
-	model.add(tf.keras.layers.Dense(10))
-	model.add(tf.keras.layers.Softmax())
+	model.add(tf.keras.layers.Dense(1, activation='sigmoid'))
 
 	# Freeze the vgg16 part of the network
 	vgg16_model.trainable = False
 
-	# set the first 25 layers (up to the last conv block)
-	# to non-trainable (weights will not be updated)
-	#for layer in vgg16_model.layers[:25]:
-	#	layer.trainable = False
-
-	# compile the model with a SGD/momentum optimizer
-	# and a very slow learning rate.
-	model.compile(optimizer=tf.keras.optimizers.SGD(lr=1e-4, momentum=0.9),
-		loss=tf.keras.losses.sparse_categorical_crossentropy,
+	model.compile(optimizer=tf.keras.optimizers.RMSprop(lr=2e-5),
+		loss = tf.keras.losses.binary_crossentropy,
 		metrics=['accuracy'])
 
-	# print model architecture
+	# Print model architecture.
 	print(model.summary())
 
-	model.fit(x_train, y_train, 
-				 epochs=1, 
-				 #batch_size=32)
-				 steps_per_epoch=60000)
-	
-	# print test loss and metric
-	print(model.evaluate(x_test, y_test))
-	
-	# save model
-	model_name = 'fine_tune_mnist_model.h5'
+	# Fit model using the generator method.
+	model.fit_generator(
+		train_generator,
+		steps_per_epoch=100,
+		epochs=30,
+		validation_data=validation_generator,
+		validation_steps=50)
+
+	print('Model evalaution:')
+	print(model.evaluate_generator(test_generator))
+
+	# Save model.
+	print('Save model')
+	model_name = 'cats_and_dogs_small_model.h5'
 	model.save(model_name)
+
+	# Load model. This fails.
+	print('Load model')
+	loaded_model = tf.keras.models.load_model(model_name)
+
+	# Print test loss and metric for loaded model.
+	print('Loaded model evalaution:')
+	print(loaded_model.evaluate_generator(test_generator))
 
 if __name__ == "__main__":
 	tf.app.run()
