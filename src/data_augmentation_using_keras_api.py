b1186f47e948aa9ca75d0990f5f661a0e8db46d4
mdabros <mdabros@users.noreply.github.com>
2018-10-07 11:07:36 +0200

    Add augmentation to data_augmentation_using_keras_api

--- src/data_augmentation_using_keras_api.py
+++ src/data_augmentation_using_keras_api.py
@@ -1,14 +1,22 @@
 import tensorflow as tf
-import tensorflow_hub as hub
-import numpy as np
 import os
 
 tf.logging.set_verbosity(tf.logging.INFO)
 
 def main(unused_argv):
 
-	# All images will be rescaled by 1./255
-	train_datagen = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1./255)
+	# All images will be rescaled by 1./255, 
+	# and random augmentation are added to the training generator
+	train_datagen = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1./255,
+		rotation_range=40,
+		width_shift_range=0.2,
+		height_shift_range=0.2,
+		shear_range=0.2,
+		zoom_range=0.2,
+		horizontal_flip=True,)
+
+	# Note that validation and test generators must not use augmentations!
+	validation_datagen = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1./255)
 	test_datagen = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1./255)
 
 	train_generator = train_datagen.flow_from_directory(
@@ -20,14 +28,20 @@ def main(unused_argv):
 		# Since we use binary_crossentropy loss, we need binary labels
 		class_mode='binary')
 
-	validation_generator = test_datagen.flow_from_directory(
+	validation_generator = validation_datagen.flow_from_directory(
 		r'E:\DataSets\CatsAndDogs\train\validation',
 		target_size=(150, 150),
 		batch_size=20,
 		class_mode='binary')
 
+	test_generator = test_datagen.flow_from_directory(
+		r'E:\DataSets\CatsAndDogs\train\test',
+		target_size=(150, 150),
+		batch_size=20,
+		class_mode='binary')
+
+	# Define simple cnn.
 	model = tf.keras.models.Sequential()
-	
 	model.add(tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(150, 150, 3)))
 	model.add(tf.keras.layers.MaxPooling2D((2, 2)))
 	model.add(tf.keras.layers.Conv2D(64, (3, 3), activation='relu'))
@@ -44,34 +58,32 @@ def main(unused_argv):
 				  loss = tf.keras.losses.binary_crossentropy,
 				  metrics=['accuracy'])
 
-	# print model architecture
+	# Print model architecture.
 	print(model.summary())
 
-	#model.fit(x_train, y_train, epochs=1, batch_size=32)
-	history = model.fit_generator(
-      train_generator,
-      steps_per_epoch=100,
-      epochs=30,
-      validation_data=validation_generator,
-      validation_steps=50)
+	# Fit model using the generator method.
+	model.fit_generator(
+		train_generator,
+		steps_per_epoch=100,
+		epochs=100,
+		validation_data=validation_generator,
+		validation_steps=50)
 
-	acc = history.history['acc']
-	val_acc = history.history['val_acc']
-	loss = history.history['loss']
-	val_loss = history.history['val_loss']
+	print('Model evalaution:')
+	print(model.evaluate_generator(test_generator))
 
-	print("acc: " + acc + "val_acc:" + val_acc)
-	print("loss: " + loss + "val_loss:" + val_loss)
-
-	# save model
-	model_name = 'mnist_model.h5'
+	# Save model.
+	print('Save model')
+	model_name = 'cats_and_dogs_small_model.h5'
 	model.save(model_name)
 
-	# load model. This fails.
+	# Load model. This fails.
+	print('Load model')
 	loaded_model = tf.keras.models.load_model(model_name)
 
-	# print test loss and metric for loaded model
-	print(loaded_model.evaluate(x_test, y_test))
+	# Print test loss and metric for loaded model.
+	print('Loaded model evalaution:')
+	print(loaded_model.evaluate_generator(test_generator))
 
 if __name__ == "__main__":
 	tf.app.run()
